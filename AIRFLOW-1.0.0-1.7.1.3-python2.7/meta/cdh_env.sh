b613f910b138b615070ff22e5fade6a5cdde616a
Naveen Balusu <Naveen@Clairvoyants-MacBook-Pro-4.local>
2018-10-25 13:49:50 -0700

    Updated environments variables to source

--- AIRFLOW-1.0.0-1.7.1.3-python2.7/meta/cdh_env.sh
+++ AIRFLOW-1.0.0-1.7.1.3-python2.7/meta/cdh_env.sh
@@ -1,5 +1,5 @@
 export AIRFLOW_DIR="$PARCELS_ROOT/$PARCEL_DIRNAME"
-export PATH=${AIRFLOW_DIR}/usr/bin:$PATH
+export PYTHONPATH=${AIRFLOW_DIR}/usr/lib/python2.7/site-packages:$PATH
 
 echo "Updating the shebang..."
 sed -i '1s+.*+#!'"${AIRFLOW_DIR}"'/usr/bin/python2.7+' ${AIRFLOW_DIR}/usr/bin/airflow
