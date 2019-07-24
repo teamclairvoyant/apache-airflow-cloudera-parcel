a8be25d4ba7422846618be1c3bff5750170602d8
Michael Arnold <github@razorsedge.org>
2019-07-24 15:06:36 -0400

    Fixes to crudini support.

--- docker/install_crudini.sh
+++ docker/install_crudini.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # These are picked up from the Docker ENV.
 AIRFLOW_DIR=${INSTALL_DIR}/${PARCEL_NAME}
-#PYVER=$(echo "$PYTHON_VERSION" | awk -F. '{print $1"."$2}')
+PYVER=$(echo "$PYTHON_VERSION" | awk -F. '{print $1"."$2}')
 #PYMAJVER=$(echo "$PYTHON_VERSION" | awk -F. '{print $1}')
 
 PATH="${AIRFLOW_DIR}/bin:${PATH}"
@@ -13,8 +13,19 @@ echo "*** Installing iniparse..."
 pip $PIPOPTS install iniparse
 
 echo "*** Installing crudini..."
-install -m 0755 -o root -g root crudini "${AIRFLOW_DIR}/bin/crudini"
+install -m 0755 -o root -g root crudini "${AIRFLOW_DIR}/bin/.crudini"
 
 echo "*** Updating the shebang..."
-sed -e "1s|.*|#!${AIRFLOW_DIR}/python${PYVER}|" -i "${AIRFLOW_DIR}/bin/crudini"
+sed -e "1s|.*|#!${AIRFLOW_DIR}/bin/python|" -i "${AIRFLOW_DIR}/bin/.crudini"
+
+echo "*** Installing crudini shell wrapper..."
+install -m 0755 -o root -g root /dev/null "${AIRFLOW_DIR}/bin/crudini"
+cat <<EOF >"${AIRFLOW_DIR}/bin/crudini"
+#!/bin/bash
+export PATH=${AIRFLOW_DIR}/bin:\$PATH
+export PYTHONHOME=${AIRFLOW_DIR}
+export PYTHONPATH=${AIRFLOW_DIR}/lib/python${PYVER}
+
+exec ${AIRFLOW_DIR}/bin/.crudini \$@
+EOF
 
